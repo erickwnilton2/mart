9406beed7f99b9280a2cc4116b074fcb38dc648b
erick <erickwelberth1@hotmail.com>
2024-09-28 14:00:28 -0300

    feat: implement menu header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Home() {
-  return <h1>app</h1>;
+  return (
+    <div>
+      <header className="p-3 border-b-2 border-b-[#101010]">
+        <div className="gap-3 flex items-center justify-between">
+        <h1 className="font-bold text-4xl text-[#ffffffef]">Mart</h1>
+        <strong className="hidden sm:block font-thin text-1xl border-b-2 text-[#fffffff6] border-b-[#969696]">Cardápio de um restaurante inexistente</strong>
+        <button className="p-2 bg-[#e2e8f0] text-[#000] rounded-lg hover:bg-[#ffffffbd]">Adicionar produto</button>
+        </div>
+      </header>
+      <main></main>
+    </div>
+  )
 }
