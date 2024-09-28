e13c2255df0744e806a2a45866cedf2b621c60ca
erick <erickwelberth1@hotmail.com>
2024-09-28 14:06:08 -0300

    standardize eslint standard code

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,16 @@ export default function Home() {
     <div>
       <header className="p-3 border-b-2 border-b-[#101010]">
         <div className="gap-3 flex items-center justify-between">
-        <h1 className="font-bold text-4xl text-[#ffffffef]">Mart</h1>
-        <strong className="hidden sm:block font-thin text-1xl border-b-2 text-[#fffffff6] border-b-[#969696]">Cardápio de um restaurante inexistente</strong>
-        <button className="p-2 bg-[#e2e8f0] text-[#000] rounded-lg hover:bg-[#ffffffbd]">Adicionar produto</button>
+          <h1 className="font-bold text-4xl text-[#ffffffef]">Mart</h1>
+          <strong className="hidden sm:block font-thin text-1xl border-b-2 text-[#fffffff6] border-b-[#969696]">
+            Cardápio de um restaurante inexistente
+          </strong>
+          <button className="p-2 bg-[#e2e8f0] text-[#000] rounded-lg hover:bg-[#ffffffbd]">
+            Adicionar produto
+          </button>
         </div>
       </header>
       <main></main>
     </div>
-  )
+  );
 }
