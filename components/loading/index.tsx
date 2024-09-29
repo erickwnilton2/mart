6ade5ed75647bca432fd856fb268d55328c12cd4
erick <erickwelberth1@hotmail.com>
2024-09-29 18:15:12 -0300

    start loading component

---
+++ components/loading/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Loading() {
+  return (
+    <div className="m-auto flex items-center justify-center h-screen">
+      <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-gray-500"></div>
+    </div>
+  );
+}
