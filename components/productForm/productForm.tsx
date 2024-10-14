f7482f49f808fb0da4c70ff61acaa13d1987c1ce
erick <erickwelberth1@hotmail.com>
2024-10-13 22:55:05 -0300

    feat: new product submission form feature

--- components/productForm/productForm.tsx
+++ components/productForm/productForm.tsx
@@ -39,30 +39,65 @@ export function ProductForm() {
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <form onSubmit={handleSubmit(OnSubmit)}>
-            <input {...register("title", { required: true })} />
-            {errors.title && <span>Campo obrigatório</span>}
+    <div className="mt-5">
+      <div className="m-2">
+        <form className="flex flex-col gap-5" onSubmit={handleSubmit(OnSubmit)}>
+          {(errors.title ||
+            errors.description ||
+            errors.image ||
+            errors.price) && (
+            <span className="text-red-500 font-bold underline">
+              Preencha todos os campos abaixo
+            </span>
+          )}
 
-            <input {...register("description", { required: true })} />
-            {errors.description && <span>Campo obrigatório</span>}
+          <label className="flex flex-col gap-2 text-white">
+            Nome:
+            <input
+              placeholder="Nome do produto"
+              className="p-1 text-black rounded-sm" 
+              {...register("title", { required: true })}
+            />
+          </label>
 
-            <input {...register("image", { required: true })} />
-            {errors.image && <span>Campo obrigatório</span>}
+          <label className="flex flex-col gap-2 text-white">
+            Descrição:
+            <input
+              placeholder="Descrição do produto"
+              className="p-1 text-black rounded-sm"
+              {...register("description", { required: true })}
+            />
+          </label>
 
-            <input {...register("price", { required: true })} />
-            {errors.price && <span>Campo obrigatório</span>}
+          <label className="flex flex-col gap-2 text-white">
+            Imagem
+            <input
+              placeholder="Adicionar imagem por URL"
+              className="p-1 text-black rounded-sm"
+              {...register("image", { required: true })}
+            />
+          </label>
 
-            {error && <p>{error}</p>}
-            {success && <p>Dados enviados com sucesso!</p>}
+          <label className="flex flex-col gap-2 text-white">
+            Valor do produto:
+            <input
+              placeholder="Valor do produto"
+              className="p-1 text-black rounded-sm"
+              {...register("price", { required: true })}
+            />
+          </label>
 
-            <button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? "Enviando..." : "Enviar"}
-            </button>
-          </form>
-        </div>
+          {error && <p>{error}</p>}
+          {success && <p>Dados enviados com sucesso!</p>}
+
+          <button
+            className="p-2 bg-[#181818] text-[#fff] rounded-sm hover:bg-[#181818e3]"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "adicionando..." : "Adicionar"}
+          </button>
+        </form>
       </div>
     </div>
   );
